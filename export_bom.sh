73e85cd8738fbb9d10b81cfcf91fdf56465e38b9
Tom Wong-Cornall <tom@wongcornall.com>
2018-04-20 13:50:10 +1200

    Progress with BOM-export script

--- export_bom.sh
+++ export_bom.sh
@@ -1,23 +1,69 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo "argument for bom file is required"
+	>&2 echo "error: argument for bom file is required"
 	exit 1
 fi
 
-echo "Sub-BOM, Part, Qty"; tail +2 $1 | sort -g | awk '
+echo "Item, Part, Qty, Value, Description"
+tail +2 $1 | awk '
 	BEGIN {
 		FS = ",";
+		PROCINFO["sorted_in"] = "@ind_str_asc";
+	}
+
+	function registerItem(part, parentPart, val, description) {
+		if (part == "\"\"") {
+			children["\"\""][""][""];
+			return;
+		}
+
+		if (!(parentPart in children))
+			registerItem(parentPart, "\"\"", "\"<UNKNOWN>\"", "\"<UNKNOWN>\"");
+
+		if (!(part in children))
+			children[part][""][""];
+
+		if (part in children[parentPart]) {
+			children[parentPart][part]["qty"]++;
+		} else {
+			children[parentPart][part]["qty"]         = 1;
+			children[parentPart][part]["val"]         = val;
+			children[parentPart][part]["description"] = description;
+		}
+	}
+
+	function currIDStr() {
+		s = "";
+		for (i = 0; i <= currLevel; i++) {
+			if (i != 0)
+				s = s ".";
+			s = s id[i];
+		}
+		return s;
+	}
+
+	function printItem(part, parentItem) {
+		if (part == "")
+			return;
+		id[currLevel]++;
+		print currIDStr() "\t" part "\t" children[parentItem][part]["qty"] "\t" children[parentItem][part]["val"] "\t" children[parentItem][part]["description"];
+		currLevel++;
+		for (child in children[part])
+			printItem(child, part);
+		id[currLevel] = 0;
+		currLevel--;
 	}
 
 	{
-		partcount[$6][$5]+=1;
+		registerItem($5, $6, $2, $3);
 	}
 
 	END {
-		for (i in partcount) {
-			for (j in partcount[i])
-				print i", " j", " partcount[i][j];
+		currLevel = 0;
+		id[currLevel] = 0;
+		for (part in children["\"\""]) {
+			printItem(part, "\"\"");
 		}
 	}
-' | sort -g --field-separator='"' --key=2
+'
